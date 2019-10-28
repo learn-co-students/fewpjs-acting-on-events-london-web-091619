5bbe8310ac13c7ced8c957c328c994c6284f7c91
Tim Golding <tgolding55@hotmail.com>
2019-10-28 11:47:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(){
+    let left = convertToInt(dodger.style.left)
+    dodger.style.left = convertToPx(left - 20)
+}
+
+function moveDodgerRight(){
+    let left = convertToInt(dodger.style.left)
+    dodger.style.left = convertToPx(left + 20)
+}
+
+
+function convertToInt(num){
+    return parseInt(num)
+}
+
+function convertToPx(num){
+    return `${num}px`
+}
+
+(function init(){
+    console.log("init")
+    document.addEventListener("keydown", function(e) {
+        if (e.key === "ArrowLeft"){
+            moveDodgerLeft()
+        }
+        if (e.key === "ArrowRight"){
+            moveDodgerRight()
+        }
+        console.log(init)
+
+      });
+})();
